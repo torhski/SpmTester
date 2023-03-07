babdee31a63aa8792a4794cd3fee2b84fdd2e9b3
torhski <tochy15987@gmail.com>
2023-03-07 16:28:00 +0900

    internal issue

--- Sources/SpmTester/Views/IAMDashboard.swift
+++ Sources/SpmTester/Views/IAMDashboard.swift
@@ -10,7 +10,7 @@ public struct IAMDashboard: View {
     var pageType: PageType
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
-    init(pageType: PageType) {
+    public init(pageType: PageType) {
         self.pageType = pageType
     }
     
