905cd8788dd284a11838fe73357b276a9a2697c7
torhski <tochy15987@gmail.com>
2023-03-07 16:46:57 +0900

    public

--- Sources/SpmTester/Core/Core.swift
+++ Sources/SpmTester/Core/Core.swift
@@ -6,16 +6,17 @@ public class Core {
     static private var _config: CoreConfiguration?
     private var _storage = InstallationStore()
     
-    static func initialize(config: CoreConfiguration) {
+    
+    static public func initialize(config: CoreConfiguration) {
         _config = config
     }
     
-    static func appId() -> String? {
+    static public func appId() -> String? {
         return _config?.appId
     }
     
     // @debug
-    static func log() {
+    static public func log() {
         if _config == nil {
             return
         }
