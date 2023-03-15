4f73dd620e589212e203982f3a4dfe78a4ce1e6c
torhski <tochy15987@gmail.com>
2023-03-15 15:53:34 +0900

    rename

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "SpmTester",
+    name: "iam-ios-sdk-tester",
     platforms: [.iOS(.v13), .macOS(.v13)],
     
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SpmTester",
+            name: "iam-ios-sdk-tester",
             targets: ["SpmTester"])
         
     ],
