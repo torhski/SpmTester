59d083b922cc77c2957943725a382beaadcf3570
torhski <tochy15987@gmail.com>
2023-03-23 15:28:52 +0900

    init

--- Sources/iam-ios-sdk-tester/Components/Views/AccessPoint/AccessPoint.swift
+++ Sources/iam-ios-sdk-tester/Components/Views/AccessPoint/AccessPoint.swift
@@ -4,6 +4,10 @@ import SwiftUI
 // tl (default), tr, bl, br
 
 public struct AccessPoint: View {
+    public init {
+        
+    }
+    
     public var body: some View {
         Text("AcessPoint")
     }
