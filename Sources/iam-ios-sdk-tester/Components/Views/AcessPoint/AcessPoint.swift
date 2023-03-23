51fb7751169a4d193f4a23b97fd2b56218dbbebc
torhski <tochy15987@gmail.com>
2023-03-23 12:51:26 +0900

    import test

--- Sources/iam-ios-sdk-tester/Components/Views/AcessPoint/AcessPoint.swift
+++ Sources/iam-ios-sdk-tester/Components/Views/AcessPoint/AcessPoint.swift
@@ -3,7 +3,14 @@ import SwiftUI
 // absolute position
 // tl (default), tr, bl, br
 
+
+import Flutter
+
 public struct AcessPoint: View {
+    init() {
+        
+    }
+    
     public var body: some View {
         Text("AcessPoint")
     }
